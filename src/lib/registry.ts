2dae25a7e52d324642a26d82b24b6089264a5205
kailash-bitpack <128362613+kailash-bitpack@users.noreply.github.com>
2023-04-11 13:32:18 +0530

    added version code of dapps (#41)
    * added version code of dapps
    
    * updating registry for ludo.io.memeameme.dapp, memepunks.io.memeameme.dapp, com.socialswap.lucky.dapp, com.socialswap.transfer.dapp, com.socialswap.airdrop.dapp, com.socialswap.giftexchange.dapp, com.socialswap.swap.dapp, com.socialswap.mart.dapp, me.sending.app.dapp
    
    * fixed category constraints

--- src/lib/registry.ts
+++ src/lib/registry.ts
@@ -86,6 +86,7 @@ export class DappStoreRegistry {
         "isSelfModerated",
         "language",
         "version",
+        "versionCode",
         "isListed",
         "listDate",
         "availableOnPlatform",
