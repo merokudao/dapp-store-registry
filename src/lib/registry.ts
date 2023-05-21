c23b0c8a1356a0b8db05f636bfed9a9d81bc3eab
kailash-bitpack <128362613+kailash-bitpack@users.noreply.github.com>
2023-05-21 11:53:04 +0530

    Removed version from top level as we have added with platform (#75)
    * Removed version from top level as we have added with platform
    
    * added referredBy for search
    
    * added referredBy for search
    
    * fixed typo error

--- src/lib/registry.ts
+++ src/lib/registry.ts
@@ -101,7 +101,8 @@ export class DappStoreRegistry {
         "packageId",
         "walletApiVersion",
         "subCategory",
-        "expiryDate"
+        "expiryDate",
+        "referredBy"
       ],
       searchOptions: { prefix: true }
     });
