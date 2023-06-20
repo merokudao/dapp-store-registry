5dac86f72aa4cbfffdb9442d46cdc6d207880150
harshit-bitpack <harshit.sharma@bitpack.me>
2023-06-20 22:33:21 +0530

    all fields added for review in ts interface

--- src/interfaces/reviewSchema.ts
+++ src/interfaces/reviewSchema.ts
@@ -6,5 +6,22 @@ export interface ReviewSchema {
    * Review of the app
    */
   review: string;
+  /**
+   * Rating of the app
+   * @minimum 1
+   * @maximum 5
+   * @pattern ^[1-5]$
+   */
+  rating: number;
+  /**
+   * The address of the reviewer
+   * @format address
+   * @pattern ^0x[a-fA-F0-9]{40}$
+   */
+  reviewer: string;
+  /**
+   * The name of the app
+   */
+  dappId: string;
 }
 
