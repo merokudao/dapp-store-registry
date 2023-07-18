074aa2e961ff7e33076b951c57454d745a836aa4
kailash-bitpack <128362613+kailash-bitpack@users.noreply.github.com>
2023-07-18 13:10:17 +0530

    Skit 251 (#114)
    * added developer shchema
    
    * added docs
    
    * updated schema
    
    * changed enum name
    
    * changed title first leter to uppercase

---
+++ src/interfaces/developerSchema.ts
@@ -0,0 +1,35 @@
+export enum DevIDPlatforms {
+  "github",
+  "gitlab",
+  "bitbucket"
+}
+
+export interface DeveloperSchema {
+  devId: string;
+  /**
+   * Legal name of the developer or the company
+   */
+  legalName: string;
+  images?: {
+    logo?: string;
+  };
+  cdn?: {
+    images?: {
+      logo?: string;
+    };
+  };
+  website: string;
+  credentils?: {
+    id: string;
+    type: keyof typeof DevIDPlatforms;
+  };
+  name?: string;
+  description?: string;
+  external_url?: string;
+  image?: string;
+  attributes?: {
+    trait_type: string;
+    value: string;
+    display_type: string;
+  }[];
+}
